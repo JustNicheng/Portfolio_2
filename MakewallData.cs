316040731c9822b8df17efd8ec85f8da05a14ee3
JustNicheng <137873909+JustNicheng@users.noreply.github.com>
2023-10-12 18:11:06 +0800

    Update

---
+++ MakewallData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+namespace JSNi
+{
+    public class MakewallData : ScriptableObject
+    {
+        
+        public Sprite[] spriteWallNew ;
+        public Sprite[] spriteWallOld ;
+        public Sprite[] spriteLand;
+        public Sprite[] spriteCeil;
+    }
+}
